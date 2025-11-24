1f566754b36d6c630dc9717df23ae9c78b4f600f
ZsigriPeter <145133141+ZsigriPeter@users.noreply.github.com>
2025-11-24 11:46:47 +0100

    feat: implement authentication views and serializers, add JWT support, and remove unused user app components

--- backend/auth-service/auth_service/urls.py
+++ backend/auth-service/auth_service/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import RegisterView, MeView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("me/", MeView.as_view(), name="me"),
 ]
+
---
+++ backend/auth-service/auth_service/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
